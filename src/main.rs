cfbbbb59ebb0e931238591d725d8d4ce14e65b79
Aistis Raulinaitis <sheganinans@gmail.com>
2018-05-15 19:53:54 -0700

    removed saving chains, regens chains every mimic

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ use serenity::framework::StandardFramework;
 use serenity::framework::standard::DispatchError;
 use serenity::http;
 use serenity::model::id::UserId;
-use serenity::model::user::User;
 use serenity::prelude::*;
 
 use std::collections::HashSet;
@@ -47,16 +46,6 @@ fn main() {
 
     let _ = client.start(); }
 
-fn sorted_user_ids(users: Vec<User>) -> Vec<u64> {
-    let mut users: Vec<u64> = users.iter().map(|x| x.id.0).collect();
-    quickersort::sort(&mut users);
-    users }
-
-fn fold_ids(ids: &Vec<u64>) -> String {
-    ids.iter().fold("".to_string(),
-                    |mut acc, val| { acc.push_str(&val.to_string()); acc }) }
-
-fn does_chain_exist(f: &String) -> bool { Path::new(&format!("./data/chain/{}", f)).exists() }
 fn does_raw_exist(f: &u64) -> bool { Path::new(&format!("./data/raw/{}", f)).exists() }
 
 command!(usage(_ctx, msg) { let _ = msg.reply("\nUsage:\n.\\mimic @user1 @user2..."); });
@@ -94,46 +83,34 @@ command!(mimic(_ctx, msg) {
         match &msg.mentions.clone().into_iter().filter(|x| !x.bot).collect::<Vec<_>>()[..] {
             &[] => { let _ = msg.reply("Requires non-bot @mention."); },
             ms  => {
-                let chain_id = fold_ids(&sorted_user_ids(ms.to_vec()));
-                if !does_chain_exist(&chain_id) {
-                    let _ = msg.reply("Beep Boop! This mimic doesn't exist yet! Generating..");
-                    let mut chain = Chain::of_order(2);
-                    for m in ms { chain.feed_file(&Path::new(&format!("./data/raw/{}", m.id.0)))?; }
-                    chain.save(&Path::new(&format!("./data/chain/{}", chain_id)))?;
+                let _ = msg.reply("Beep Boop! This mimic doesn't exist yet! Generating..");
+                let mut chain = Chain::of_order(2);
+                for m in ms { chain.feed_file(&Path::new(&format!("./data/raw/{}", m.id.0)))?; }
 
-                    let _ = msg.reply(&format!("Mimic {} ready! Let's see what it's like!", chain_id));
-                } else {()}
-
-                let _ = msg.reply(&format!("Loading mimic: {}", chain_id));
-                let mut chain = Chain::<String>::load(&Path::new(&format!("./data/chain/{}", chain_id)));
-
-                match chain {
-                    Err(err) => { let _ = msg.reply(&format!("ERR! {:?}", err)); },
-                    Ok(chain) => {
-                        let mimic_str = ms.iter().fold("".to_string(),
-                                                       |mut acc, val| { acc.push_str(" ");
-                                                                        acc.push_str(&val.name);
-                                                                        acc });
-                        let mut rep = "".to_string();
-                        let mut chn = chain.iter();
-                        let mut num_resp = 0;
-                        while num_resp < 10 {
-                            let line = chn.next();
-                            match line {
-                                None => {},
-                                Some(line) => {
-                                    let first_word = &line[0];
-                                    if first_word.starts_with(";;") ||
-                                       first_word.starts_with(".") ||
-                                       first_word.starts_with("!") ||
-                                       line.len() < 7
-                                        {}
+                let mimic_str = ms.iter().fold("".to_string(),
+                                               |mut acc, val| { acc.push_str(" ");
+                                                                acc.push_str(&val.name);
+                                                                acc });
+                let mut rep = "".to_string();
+                let mut chn = chain.iter();
+                let mut num_resp = 0;
+                while num_resp < 10 {
+                    let line = chn.next();
+                    match line {
+                        None => {},
+                        Some(line) => {
+                            let first_word = &line[0];
+                            if first_word.starts_with(";;") ||
+                                first_word.starts_with(".") ||
+                                first_word.starts_with("!") ||
+                                line.len() < 7
+                            {}
+                            else {
+                                for word in &line {
+                                    if word.starts_with("<@") {}
                                     else {
-                                        for word in &line {
-                                            if word.starts_with("<@") {}
-                                            else {
-                                                rep.push_str(&word);
-                                                rep.push_str(" ") }}
-                                        rep.push_str("\n\n");
-                                        num_resp += 1 }}}}
-                        let _ = msg.reply(&format!("Mimicking{}:\n\n{}", mimic_str, rep)); }}}}}});
+                                        rep.push_str(&word);
+                                        rep.push_str(" ") }}
+                                rep.push_str("\n\n");
+                                num_resp += 1 }}}}
+                let _ = msg.reply(&format!("Mimicking{}:\n\n{}", mimic_str, rep)); }}}});
